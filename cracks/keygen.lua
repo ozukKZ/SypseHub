12d43bf1ff77d0f6b94192c6a4557960458a1d09
lolasj12491294 <74175556+lolasj12491294@users.noreply.github.com>
2020-11-09 10:45:38 +0200

    Create keygen.lua

---
+++ cracks/keygen.lua
@@ -0,0 +1,26 @@
+--made by Cyclops#0001
+local sets = {
+    action = setclipboard,
+    howmuchkeys = 10,
+    keylen = 32;
+};
+
+local totalkeys = '';
+local randompattern = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; --pattern to randomize alphanumerics
+local function randomizeAlphanumeric()
+    local randomized = math.random(1, randompattern:len());
+    return randompattern:sub(randomized, randomized);
+end;
+
+local function randomkey()
+    local newkey = '';
+    for i = 1, sets.keylen, 1 do
+        newkey = newkey.. randomizeAlphanumeric();
+    end;
+    return 'key_'.. newkey;
+end;
+
+for i = 1, sets.howmuchkeys, 1 do
+    totalkeys = totalkeys .. randomkey() .. ((i == sets.howmuchkeys) and '\0' or '\n');
+end;
+sets.action(totalkeys)
